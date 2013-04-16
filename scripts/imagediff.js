d483f19ac0b27a7887fb7aed2d201bc378c4f308
MaPePeR <sorry>
2013-04-16 19:19:17 +0200

    imagediff using img.onload to wait for image.

--- scripts/imagediff.js
+++ scripts/imagediff.js
@@ -1,24 +1,36 @@
 //based on http://pastebin.com/cWxA6EDR
 
-  function diff() {
-    var c1=document.getElementById("canvas1");
-    var ctx1=c1.getContext("2d");
-    var img1=new Image();
-    img1.src = 'images/'+currentFrame+'.png';
+  var c1=document.getElementById("canvas1");
+  var ctx1=c1.getContext("2d");
+  var img1=new Image();
+
+  var c2=document.getElementById("canvas2");
+  var ctx2=c2.getContext("2d");
+  var img2=new Image();
+
+  var loadedcount=0;
+
+  function drawDiffImageWhenBothAreLoaded() {
+    loadedcount++
+    if(loadedcount>=2)
+    {
+      loadedcount=0;
+      drawDiffImage();
+    }
+  }
+  img1.onload=drawDiffImageWhenBothAreLoaded;
+  img2.onload=drawDiffImageWhenBothAreLoaded;
+
+
+  var c3=document.getElementById("canvas3");
+  var ctx3=c3.getContext("2d");
+  function drawDiffImage() {
     ctx1.drawImage(img1,0,0);
-    var imageData1 = ctx1.getImageData(0, 0, c1.width, c1.height);
-    var data1 = imageData1.data;
-    
-    var c2=document.getElementById("canvas2");
-    var ctx2=c2.getContext("2d");
-    var img2=new Image();
-    var nextframe = currentFrame+1;
-    img2.src='images/' + nextframe + '.png';
+    var data1 = ctx1.getImageData(0, 0, c1.width, c1.height).data;
+
     ctx2.drawImage(img2,0,0);
     var data2 = ctx2.getImageData(0, 0, c2.width, c2.height).data;
-    
-    var c3=document.getElementById("canvas3");
-    var ctx3=c3.getContext("2d");
+
     var imageData = ctx3.getImageData(0, 0, c3.width, c3.height);
     var data3 = imageData.data;
     for(var i = 0, n = data1.length; i < n; i += 4) {
@@ -33,4 +45,12 @@
     }
     
     ctx3.putImageData(imageData, 0, 0);
+
+  }
+  function diff() {
+    loadedcount=0;
+    img1.src = 'images/'+currentFrame+'.png';
+
+    var nextframe = currentFrame+1;
+    img2.src='images/' + nextframe + '.png';
   }
