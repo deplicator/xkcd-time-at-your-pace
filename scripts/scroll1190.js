96ecef6e4c27a83586f551a5a8bcfeda06b55a5c
MaPePeR <mapeper@example.com>
2013-05-01 14:42:37 +0200

    Added code to pause playback on special frames.
    Also made 403 a special frame.

--- scripts/scroll1190.js
+++ scripts/scroll1190.js
@@ -3,7 +3,7 @@
  * give credit where it is due.
  */
 
-var specialframes = [52, 170, 175, 320, 408, 414, 486, 487, 488, 490, 562, 563, 564, 637, 638, 640, 641, 642, 659, 660, 661, 832, 833, 834, 835, 838, 839, 855, 856, 857, 859, 860, 861, 862, 864, 865, 985, 1004, 1006];
+var specialframes = [52, 170, 175, 320, 403, 408, 414, 486, 487, 488, 490, 562, 563, 564, 637, 638, 640, 641, 642, 659, 660, 661, 832, 833, 834, 835, 838, 839, 855, 856, 857, 859, 860, 861, 862, 864, 865, 985, 1004, 1006];
 var images = [];
 var bitlydata = null;
 var imageslen = 0;
@@ -329,9 +329,39 @@ if(BrowserDetect.browser != "Firefox") {
 }
 
 
+// Thanks to jfriend00 on http://stackoverflow.com/questions/10264239/fastest-way-to-determine-if-an-element-is-in-a-sorted-array
+function binary_search_iterative(a, value) {
+    var lo = 0, hi = a.length - 1, mid;
+    while (lo <= hi) {
+        mid = Math.floor((lo+hi)/2);
+        if (a[mid] > value)
+            hi = mid - 1;
+        else if (a[mid] < value)
+            lo = mid + 1;
+        else
+            return mid;
+    }
+    return null;
+}
+
+function isSpecial(frame) {
+    return binary_search_iterative(specialframes,frame) != null;
+}
+
 //Autoplay stuff
+var specialframecounter = 0;
 var speed = 1000;
 var timer = $.timer(function() {
+    if(isSpecial(currentFrame)) {
+        specialframecounter+=speed;
+        if(specialframecounter < (parseInt($('#PauseSpecialFrameAmount').val(),10) || 0)*1000) {
+            return;//should pause x seconds... 
+        }
+        else {
+            specialframecounter=0;
+        }
+    }
+
     //https://code.google.com/p/jquery-timer/
     updateAll(nextslideindex);
     nextslideindex++;
