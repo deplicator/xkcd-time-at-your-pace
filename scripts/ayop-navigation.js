deplicator/xkcd-time-at-your-pace

commit dda0e670f4f0ec6e35d7468fcac632e31b6071f7

Tinkered with manual frame input, arrow keys work now but data validation broke.

scripts/ayop-navigation.js
@@ -68,11 +68,13 @@ function nextSpecialFrame() {
  *       ctrl+shift+right arrow move to last frame
  */
 $(document).keydown(function (e) {
-    if (e.which == 37) { //left-key-pressed
-        prevFrame();
-    } else if (e.which == 39) { //right-key-pressed
-        nextFrame();
-    }
+	if(!($("#manualinput").is(":focus"))) {
+		if (e.which == 37) { //left-key-pressed
+			prevFrame();
+		} else if (e.which == 39) { //right-key-pressed
+			nextFrame();
+		}
+	}
 });
 
 
@@ -197,21 +199,10 @@ $(document).ready(function () {
     
     //Changes frame as number is typed in frame
     $('#manualinput').keyup(function() {
-        var manualinput = $('#manualinput').val();
-        if(manualinput < 1) {
-            updateAll(1);
-        } else if(manualinput > frameCount) {
-            updateAll(frameCount);
-        } else {
-            updateAll(parseInt(manualinput));
-        }
-    });
-    
-    //Doesn't allow letters in the text box
-    //http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
-    $("#manualinput").keydown(function(event) {
-        // Allow: backspace, delete, tab, escape, and enter
-        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+
+		//Doesn't allow letters in the text box
+		//http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+		if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
              // Allow: Ctrl+A
             (event.keyCode == 65 && event.ctrlKey === true) || 
              // Allow: home, end, left, right
@@ -223,11 +214,18 @@ $(document).ready(function () {
             // Ensure that it is a number and stop the keypress
             if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
                 event.preventDefault(); 
-            }   
+            }
         }
-    });
-    
-    
+        
+        var manualinput = $('#manualinput').val();
+        if(manualinput < 1) {
+            updateAll(1);
+        } else if(manualinput > frameCount) {
+            updateAll(frameCount);
+        } else {
+            updateAll(parseInt(manualinput));
+        }
+    });    
 
 
     /*

scripts/ayop-cookies.js
@@ -1,2 +1,1 @@
 
-$.cookie('ayop-fdpanel', '1');

scripts/ayop-ui.js
@@ -20,7 +20,16 @@ function loadPastPanel(num, container) {
     }
 }
 
+//Select all text in a text input
+function textSelect() {
+	$(this).select();
+}
+
+
 $(document).ready(function () {
+    $('#manualinput').click(textSelect);
+    $('#actuallink').click(textSelect);
+    $('#freezeframe').click(textSelect);
     
     if(panels == 'closed') {
         $('#framedata .inside').hide('fast', function() {

commit 7727806aaafbcb6f51271f2104e98e3b9f24b532

Fixes input validation for manualinput (broke in dda0e67)
cc #60

scripts/ayop-navigation.js
@@ -198,7 +198,7 @@ $(document).ready(function () {
     });
     
     //Changes frame as number is typed in frame
-    $('#manualinput').keyup(function() {
+    $('#manualinput').keyup(function(event) {
 
 		//Doesn't allow letters in the text box
 		//http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery