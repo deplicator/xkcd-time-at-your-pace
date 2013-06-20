d2303cf7e22a6125304868e1e920594490e80db2
Rob Smith <kormoc@gmail.com>
2013-06-20 16:43:08 -0700

    Add in a utility script to download historic frames to the local machine to help bootstrap development work.

---
+++ downloadFrames.php
@@ -0,0 +1,65 @@
+<?php
+/* 
+ * downloadFrames.php
+ * Downloads frames from the public site to mirror locally
+ */
+
+header('Content-Type: text/plain');
+include('config.php');
+
+$DBH = new PDO(PDO_CONNECTION, DB_WRITE_USER, DB_WRITE_PASS);
+
+$insertFrame = $DBH->prepare('INSERT INTO Frames (`update`, `frame`, `link`, `llink`, `blink`, `special`) VALUES (FROM_UNIXTIME(?), ?, ?, ?, ?, ?)');
+
+$frames = json_decode(file_get_contents("http://xkcd.mscha.org/time.json"), true);
+
+if (!$frames)
+	die("Frame data not valid!\n\n");
+	
+foreach ($frames as $frame) {
+	// Ugh, ensure it's a int, some frames end in an a?
+	$frame['frameNo'] += 0;
+
+	echo "\r";
+	echo str_pad("", 80, " ");
+	echo "\r";
+	
+	echo "Processing {$frame['frameNo']}... ";
+	
+	if ($frame['frameNo'] == 1)
+		$frame['hash'] = 1;
+	$frame['path'] = "./data/frames/{$frame['frameNo']}.png";
+	$frame['llink'] = "http://geekwagon.net/projects/xkcd1190/data/frames/{$frame['frameNo']}.png";
+	$frame['blink'] = "";
+	$frame['special'] = 0;
+	$frame['dateTime'] = strtotime($frame['dateTime']);
+	
+	if (!file_exists($frame['path'])) {
+		echo "Downloading... ";
+		$fp = fopen($frame['path'], 'w');
+		$ch = curl_init($frame['downloadedUrl']);
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		fclose($fp);
+		chmod($frame['path'], 0444);
+	}
+	
+	$sh = $DBH->prepare('SELECT * FROM frames WHERE frame = ?');
+	$sh->execute(array($frame['frameNo']));
+	$res = $sh->fetch(PDO::FETCH_ASSOC);
+	
+	if (!$res) {
+		echo "Inserting... ";
+		
+		$data = array($frame['dateTime'],
+					  $frame['frameNo'],
+					  $frame['downloadedUrl'],
+					  $frame['llink'],
+					  $frame['blink'],
+					  $frame['special']);
+		$res = $insertFrame->execute($data);
+	}
+}
+
+echo "\n\n";
